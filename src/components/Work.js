e23071a8ee0132259be506d09b5040a5b10176e5
anagh534 <anaghkrkkl@gmail.com>
2026-08-18 12:50:45 +0530

    Add new projects to Work component with relevant details and images

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,6 +11,33 @@ const ITEMS = [
     tags: ["Node.js", "MongoDB", "Next.js", "TypeScript", "Postgres"],
     imageUrl: "/theliv.png",
     url: "http://www.theliv.co.uk/", // optional
+  },
+  {
+    slug: "MyIndianStore - E-commerce Platform",
+    title: "MyIndianStore — E-commerce Platform",
+    meta: "A platform for Indian products to be sold online. It is a multi-vendor platform where vendors can sell their products and customers can buy them.",
+    year: "2025",
+    tags: ["Laravel", "MySQL", "Flutter", "Tailwind CSS", "AWS"],
+    imageUrl: "/myindianstore-900.webp",
+    url: "https://myindianstore.app/", // optional
+  },
+  {
+    slug: "SERENE ABODE - REAL ESTATE PLATFORM",
+    title: "SERENE ABODE — Real Estate Platform",
+    meta: "A platform for real estate agents to manage their properties and clients. It is a multi-agent platform where agents can list their properties and clients can contact them.",
+    year: "2024",
+    tags: ["Next.js", "Nest.js", "MongoDB", "Postgres", "Digital Ocean"],
+    imageUrl: "/serene-abode-900.webp",
+    url: "https://www.sereneabode.ca/", // optional
+  },
+  {
+    slug: "Bhavayami Nritalaya - Dance Academy",
+    title: "Bhavayami Nritalaya — Dance Academy",
+    meta: "A platform for dance academy to manage their students and classes. It is a multi-instructor platform where instructors can manage their classes and students can enroll in them.",
+    year: "2025",
+    tags: ["HTML", "CSS", "JavaScript", "Google Sheets Api", "GitHub Pages"],
+    imageUrl: "/bhavayami-nritalaya-900.webp",
+    url: "https://www.bhavayaminritalaya.in/", // optional
   }
 ];
 
